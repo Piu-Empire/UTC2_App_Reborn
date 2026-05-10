c9057e1053b08a2f4592e8ac6122a1c178887a41
Đinh Hoàng Phúc <dinhhoangphuc264@gmail.com>
2026-05-10 12:48:50 +0700

    Update MainActivity.java

--- app/src/main/java/com/utc2/appreborn/ui/main/MainActivity.java
+++ app/src/main/java/com/utc2/appreborn/ui/main/MainActivity.java
@@ -12,16 +12,14 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.utc2.appreborn.R;
 import com.utc2.appreborn.ui.components.LiquidBarKt;
-<<<<<<< HEAD
 import com.utc2.appreborn.ui.courseregistration.CourseRegistrationFragment;
-=======
->>>>>>> c48bce4bd92c562902523d93bc66f93c2cb31190
 import com.utc2.appreborn.ui.home.HomeFragment;
 import com.utc2.appreborn.ui.profile.ProfileFragment;
 import com.utc2.appreborn.ui.public_services.PublicServiceFragment;
 import com.utc2.appreborn.ui.results.AcademicResultsFragment;
 import com.utc2.appreborn.ui.schedule.ScheduleFragment;
 import com.utc2.appreborn.ui.tuition.TuitionFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     // Tags để quản lý Fragment, tránh tạo mới liên tục gây tốn RAM
@@ -82,18 +80,8 @@ public class MainActivity extends AppCompatActivity {
             switchTab(ScheduleFragment.class, TAG_SCHEDULE);
 
         } else if (id == R.id.nav_register) {
-<<<<<<< HEAD
+
             switchTab(CourseRegistrationFragment.class, "tag_register");
-=======
-
-            Intent intent = new Intent(
-                    this,
-                    com.utc2.appreborn.ui.courseregistration
-                            .CourseRegistrationActivity.class
-            );
-
-            startActivity(intent);
->>>>>>> c48bce4bd92c562902523d93bc66f93c2cb31190
 
         } else if (id == R.id.nav_result) {
 
